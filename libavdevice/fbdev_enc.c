d4c1e38533491472599cd712eecd7d8b40b7bde7
Michael Niedermayer <michaelni@gmx.at>
2013-10-24 10:18:17 +0200

    avdevice/fbdev_enc: avoid declaring variables in for()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/fbdev_enc.c
+++ libavdevice/fbdev_enc.c
@@ -48,8 +48,9 @@ static av_cold int fbdev_write_header(AVFormatContext *h)
     AVStream *st = NULL;
     int ret, flags = O_RDWR;
     char errbuf[128];
+    int i;
 
-    for (int i = 0; i < h->nb_streams; i++) {
+    for (i = 0; i < h->nb_streams; i++) {
         if (h->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
             if (!st) {
                 fbdev->index = i;
@@ -123,6 +124,7 @@ static int fbdev_write_packet(AVFormatContext *h, AVPacket *pkt)
     int bytes_per_pixel = ((codec_ctx->bits_per_coded_sample + 7) >> 3);
     int src_line_size = video_width * bytes_per_pixel;
     char errbuf[128];
+    int i;
 
     if (fbdev->index != pkt->stream_index)
         return 0;
@@ -183,7 +185,7 @@ static int fbdev_write_packet(AVFormatContext *h, AVPacket *pkt)
         }
     }
 
-    for (int i = 0; i < disp_height; i++) {
+    for (i = 0; i < disp_height; i++) {
         memcpy(pout, pin, bytes_to_copy);
         pout += fbdev->fixinfo.line_length;
         pin  += src_line_size;
